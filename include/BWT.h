8d70909d6ee6ebc811ebbe4c511db41be5ad1612
ezuryy <daria.kidinova@gmail.com>
2022-01-09 11:38:13 +0300

    include algorithm

--- include/BWT.h
+++ include/BWT.h
@@ -1,8 +1,9 @@
-#pragma once
+#ifndef MY_PROJECT_BWT_H
+#define MY_PROJECT_BWT_H
 
-#include <functions.h>
-
+#include <algorithm>
 #include <unordered_map>
+#include <string>
 
 using std::pair;
 using std::string;
@@ -12,3 +13,5 @@ pair<string, size_t> BWT_encode(const string& input);
 string BWT_decode(const size_t& k, const string& last);
 
 string BWT_decode_optimized(const size_t& k, const string& last);
+
+#endif  // MY_PROJECT_BWT_H
