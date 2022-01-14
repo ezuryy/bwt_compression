a08a6822b952adbd0329132d72a4158f0b9da267
ezuryy <daria.kidinova@gmail.com>
2022-01-14 17:04:00 +0300

    add function for testing

---
+++ include/test.h
@@ -0,0 +1,9 @@
+#ifndef MY_PROJECT_TEST_H
+#define MY_PROJECT_TEST_H
+
+#include "functions.h"
+
+void CompressDecompressTest(const string& test_filename, const string& compressed_filename,
+                            const string& decompressed_filename, const int& expected_ratio);
+
+#endif //MY_PROJECT_TEST_H
--- source/test.cpp
+++ source/test.cpp
@@ -1,12 +1,9 @@
 #include <gtest/gtest.h>
 
-#include "functions.h"
+#include "test.h"
 
-TEST(Test1, textAboutInternetAndMind) {
-    string test_filename = "../tests/text001.txt";
-    string compressed_filename = "../tests/compressed001.txt";
-    string decompressed_filename = "../tests/decompressed001.txt";
-
+void CompressDecompressTest(const string& test_filename, const string& compressed_filename,
+                            const string& decompressed_filename, const int& expected_ratio) {
     CompressFile(test_filename, compressed_filename);
     ExpandFile(compressed_filename, decompressed_filename);
 
@@ -32,7 +29,15 @@ TEST(Test1, textAboutInternetAndMind) {
     EXPECT_EQ(test_input, decompressed_input);
 
     size_t ratio = CompressionRatio(test_filename, compressed_filename);
-    EXPECT_TRUE(ratio >= 160);
+    EXPECT_TRUE(ratio >= expected_ratio);
+}
+
+TEST(Test1, textAboutInternetAndMind) {
+    string test_filename = "../tests/text001.txt";
+    string compressed_filename = "../tests/compressed001.txt";
+    string decompressed_filename = "../tests/decompressed001.txt";
+
+    CompressDecompressTest(test_filename, compressed_filename, decompressed_filename, 160);
 }
 
 TEST(Test2, repeatedFourTimesText) {
@@ -40,32 +45,7 @@ TEST(Test2, repeatedFourTimesText) {
     string compressed_filename = "../tests/compressed002.txt";
     string decompressed_filename = "../tests/decompressed002.txt";
 
-    CompressFile(test_filename, compressed_filename);
-    ExpandFile(compressed_filename, decompressed_filename);
-
-    std::ifstream test_f(test_filename, std::ios::in);
-    if (!test_f.is_open()) {
-        throw std::invalid_argument("can't open file " + test_filename);
-    }
-    std::string line, test_input;
-    while (std::getline(test_f, line)) {
-        test_input += (line + '\n');
-        line.clear();
-    }
-
-    std::ifstream decompressed_f(decompressed_filename, std::ios::in);
-    if (!decompressed_f.is_open()) {
-        throw std::invalid_argument("can't open file " + decompressed_filename);
-    }
-    std::string decompressed_input;
-    while (std::getline(decompressed_f, line)) {
-        decompressed_input += (line + '\n');
-    }
-
-    EXPECT_EQ(test_input, decompressed_input);
-
-    size_t ratio = CompressionRatio(test_filename, compressed_filename);
-    EXPECT_TRUE(ratio >= 250);
+    CompressDecompressTest(test_filename, compressed_filename, decompressed_filename, 250);
 }
 
 TEST(Test3, textAboutLZW) {
@@ -73,32 +53,7 @@ TEST(Test3, textAboutLZW) {
     string compressed_filename = "../tests/compressed003.txt";
     string decompressed_filename = "../tests/decompressed003.txt";
 
-    CompressFile(test_filename, compressed_filename);
-    ExpandFile(compressed_filename, decompressed_filename);
-
-    std::ifstream test_f(test_filename, std::ios::in);
-    if (!test_f.is_open()) {
-        throw std::invalid_argument("can't open file " + test_filename);
-    }
-    std::string line, test_input;
-    while (std::getline(test_f, line)) {
-        test_input += (line + '\n');
-        line.clear();
-    }
-
-    std::ifstream decompressed_f(decompressed_filename, std::ios::in);
-    if (!decompressed_f.is_open()) {
-        throw std::invalid_argument("can't open file " + decompressed_filename);
-    }
-    std::string decompressed_input;
-    while (std::getline(decompressed_f, line)) {
-        decompressed_input += (line + '\n');
-    }
-
-    EXPECT_EQ(test_input, decompressed_input);
-
-    size_t ratio = CompressionRatio(test_filename, compressed_filename);
-    EXPECT_TRUE(ratio >= 145);
+    CompressDecompressTest(test_filename, compressed_filename, decompressed_filename, 145);
 }
 
 TEST(Test4, simpleString) {
@@ -106,29 +61,5 @@ TEST(Test4, simpleString) {
     string compressed_filename = "../tests/compressed004.txt";
     string decompressed_filename = "../tests/decompressed004.txt";
 
-    CompressFile(test_filename, compressed_filename);
-    ExpandFile(compressed_filename, decompressed_filename);
-
-    std::ifstream test_f(test_filename, std::ios::in);
-    if (!test_f.is_open()) {
-        throw std::invalid_argument("can't open file " + test_filename);
-    }
-    std::string line, test_input;
-    while (std::getline(test_f, line)) {
-        test_input += (line + '\n');
-        line.clear();
-    }
-
-    std::ifstream decompressed_f(decompressed_filename, std::ios::in);
-    if (!decompressed_f.is_open()) {
-        throw std::invalid_argument("can't open file " + decompressed_filename);
-    }
-    std::string decompressed_input;
-    while (std::getline(decompressed_f, line)) {
-        decompressed_input += (line + '\n');
-    }
-
-    EXPECT_EQ(test_input, decompressed_input);
-    size_t ratio = CompressionRatio(test_filename, compressed_filename);
-    EXPECT_TRUE(ratio >= 150);
+    CompressDecompressTest(test_filename, compressed_filename, decompressed_filename, 150);
 }
